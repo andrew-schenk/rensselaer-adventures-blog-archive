0ae3fe292c2c1076684bdccdaa6f8be079449145
Andrew Schenk <andrew.schenk.dev@gmail.com>
2025-04-19 12:15:15 -0500

    working on dynamic data ingestion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
+'use client';
+
 import Image from "next/image";
 import styles from "./page.module.css";
 import data from "./data.json";
+import {useState} from 'react'
 
 export default function Home() {
+
+
+  const [activeExpandables, setActiveExpandables] = useState<{years: string[], yearsAndMonths: string[]}>({years: [], yearsAndMonths: []});
+
+  const toggleSection = ( year: string ) => {
+    console.log(year);
+  
+    const tmpArrYears = [...activeExpandables.years];
+    tmpArrYears.push(year);
+    const tmpExpandables = { years: tmpArrYears, yearsAndMonths: activeExpandables.yearsAndMonths };
+    setActiveExpandables(tmpExpandables);
+  }
+
+  const isYearActive = (year: string): boolean => {
+
+    return activeExpandables.years.indexOf(year) >= 0;
+  }
+  
+  const isYearAndMonthActive = (year: string, month: string): boolean => {
+  
+    return false;
+  }
+
+
   return (
     <div className={styles.main}>
       <Image
@@ -22,6 +49,39 @@ export default function Home() {
       </div>
 
       <div className={styles.title}>Archive</div>
+      {getYears().map((year: string) => {
+        
+        return (
+          <div>
+            <div className={`${styles.year} ${isYearActive(year.year) ? styles.active : ''}`} onClick={() => toggleSection(year.year)} key={year.year}>{year.year}</div>
+            {isYearActive(year.year) ?
+            <div className={styles.months}></div>
+            : null}
+            
+          </div>
+        )
+      })}
+      
     </div>
   );
 }
+
+
+const getYears = () => {
+  return data.reduce((a, {year}) =>
+    a.some(ob => ob.year === year) ? a : a.concat({year})
+  , []);
+}
+
+const getMonthsForYear = (year: string) => {
+
+
+  const postsForYear = data.filter((post) => post.year === year);
+
+  const monthsForYear = postsForYear.reduce((accumulator, {month}) => {
+    return accumulator.some(obj => obj.month === month) ? accumulator : accumulator.concat({month})
+  })
+
+  return monthsForYear;
+}
+
